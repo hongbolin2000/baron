5c7c57032a018f16a0f7e7a37b1980fac499567c
berlin <bolin.hong@outlook.com>
2025-02-25 11:56:47 +0800

    布局代码规范化

--- baron-core/src/main/java/com/hongyou/baron/util/XmlUtil.java
+++ baron-core/src/main/java/com/hongyou/baron/util/XmlUtil.java
@@ -18,10 +18,17 @@ import java.util.List;
  */
 public class XmlUtil extends cn.hutool.core.util.XmlUtil {
 
+    /**
+     * 私有构造函数
+     */
     private XmlUtil() {}
 
     /**
      * 解析子元素
+     *
+     * @param parent 父元素
+     * @param tagName 标签名
+     * @return 子元素集合
      */
     public static List<Element> getChildElements(final Element parent, final String tagName) {
         List<Element> elements = new ArrayList<>();
@@ -38,11 +45,27 @@ public class XmlUtil extends cn.hutool.core.util.XmlUtil {
     }
 
     /**
-     * 解析属性
+     * 解析字符属性
+     *
+     * @param element 待解析的元素
+     * @param name 属性名
+     * @return 有值时直接返回，无值返回空串
      */
     public static String getAttribute(final Element element, final String name) {
         String value = element.getAttribute(name);
-        return StrUtil.isBlank(value) ? "" : value;
+        return StrUtil.isBlank(value) ? StrUtil.EMPTY : value;
+    }
+
+    /**
+     * 解析字符属性
+     *
+     * @param element 待解析的元素
+     * @param name 属性名
+     * @return 有值时直接返回，无值返回传入的默认值
+     */
+    public static String getAttribute(final Element element, final String name, final String defaultValue) {
+        String value = element.getAttribute(name);
+        return StrUtil.isBlank(value) ? defaultValue : value;
     }
 
     /**
