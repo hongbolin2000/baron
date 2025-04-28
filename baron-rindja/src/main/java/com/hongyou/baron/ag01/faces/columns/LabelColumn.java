f02f2de90193a4b1134fa0882ddfc40a44274e51
berlin <bolin.hong@outlook.com>
2025-04-28 12:25:55 +0800

    表格总结栏

--- baron-rindja/src/main/java/com/hongyou/baron/ag01/faces/columns/LabelColumn.java
+++ baron-rindja/src/main/java/com/hongyou/baron/ag01/faces/columns/LabelColumn.java
@@ -15,7 +15,11 @@
  */
 package com.hongyou.baron.ag01.faces.columns;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.hongyou.baron.ag01.Environment;
 import com.hongyou.baron.ag01.faces.AbstractColumn;
+import com.hongyou.baron.util.XmlUtil;
 import org.w3c.dom.Element;
 
 /**
@@ -26,11 +30,29 @@ import org.w3c.dom.Element;
 public class LabelColumn extends AbstractColumn {
 
     /**
+     * 是否总结栏
+     */
+    private final boolean summary;
+
+    /**
      * 加载表格标签列定义
      *
      * @param element 表格标签列元素定义
      */
     protected LabelColumn(final Element element) {
         super(element);
+        summary = XmlUtil.getAttributeAsBool(element, "summary", false);
+    }
+
+    /**
+     * 生成控件定义
+     *
+     * @param env 运行参数
+     */
+    @Override
+    public JsonNode generate(Environment env) {
+        ObjectNode root = (ObjectNode) super.generate(env);
+        root.put("summary", this.summary);
+        return root;
     }
 }
